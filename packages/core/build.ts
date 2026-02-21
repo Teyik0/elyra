43ec3c4f8a3bf5b7c0400e0ff12ba3516c59b5d4
teyik0 <teyik0.fr@icloud.com>
2026-02-21 14:57:36 +0100

    feat: add lib build to elysion

--- packages/core/build.ts
+++ packages/core/build.ts
@@ -5,11 +5,10 @@ $.cwd(import.meta.dir);
 await $`rm -rf dist`;
 await $`tsc --project tsconfig.dts.json`;
 
-// JS ESM with Bun.build (for Node/Deno)
 await Bun.build({
   entrypoints: [`${import.meta.dir}/src/elysion.ts`, `${import.meta.dir}/src/client.ts`],
   outdir: `${import.meta.dir}/dist`,
-  target: "node",
+  target: "bun",
   format: "esm",
   external: ["elysia", "react", "react-dom"],
   minify: false,
